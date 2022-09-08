cb40798758293bd53d49ea454c53866adf3b5fe3
nandakho <anandakhosuri@yahoo.com>
2022-09-08 11:28:58 +0700

    Remove cordova file plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { File } from '@awesome-cordova-plugins/file/ngx'
 import { CameraComponentModule } from "./components/camera/camera.module";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, CameraComponentModule],
   providers: [
-    File, 
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent],
